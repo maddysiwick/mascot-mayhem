905af83f97c9ede46f1595bb0aed4dafac869b19
m-bernardin <mjrb4mjrb@gmail.com>
2025-11-04 12:09:27 -0500

    Game creation + classes outline

---
+++ Game/Mascot-Mayhem/Arena.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Arena here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Arena extends World
+{
+
+    /**
+     * Constructor for objects of class Arena.
+     * 
+     */
+    public Arena()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
---
+++ Game/Mascot-Mayhem/Gnu.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Gnu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Gnu extends Player
+{
+    /**
+     * Act - do whatever the Gnu wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
