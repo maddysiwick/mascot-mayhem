302d91b7bf64737303358f8e251f5d39c8278f05
m-bernardin <mjrb4mjrb@gmail.com>
2025-11-29 20:53:49 -0500

    bio system works

--- Game/Mascot-Mayhem/CharacterSelect.java
+++ Game/Mascot-Mayhem/CharacterSelect.java
@@ -107,9 +107,15 @@ public class CharacterSelect extends World
         addObject(duke,getWidth()/2+290,550);
         duke.makeStatic();
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         Gnu gnu=new Gnu(true,false,0,false);
         addObject(gnu,getWidth()+410,525);
         gnu.makeStatic();
+=======
+        dukeBio=duke.getBio();
+        wilburBio="";
+        gnuBio="";
+>>>>>>> Stashed changes
 =======
         dukeBio=duke.getBio();
         wilburBio="";
